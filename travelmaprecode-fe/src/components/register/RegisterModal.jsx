987d435f410a97cb7e9f353269440c8d5f3235b6
Jino <adg0609@naver.com>
2022-07-12 00:28:44 +0900

    검증 로직

--- travelmaprecode-fe/src/components/register/RegisterModal.jsx
+++ travelmaprecode-fe/src/components/register/RegisterModal.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Modal, Button, Form, Container} from "react-bootstrap";
 
 import './RegisterModal.css'
 
 const RegisterModal = ({show, onHide}) => {
+    const [nickname, setNickname] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const onNicknameHandler = (event) => {
+        setNickname(event.currentTarget.value)
+    }
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if (password !== confirmPassword) {
+            return alert('비밀번호와 비밀번호확인은 같아야 합니다.')
+        }
+    }
 
     return (
         <Modal
@@ -21,25 +47,25 @@ const RegisterModal = ({show, onHide}) => {
                     <Form>
                         <Form.Group>
                             <Form.Label>닉네임</Form.Label>
-                            <Form.Control placeholder="Nickname"/>
+                            <Form.Control value={nickname} onChange={onNicknameHandler} placeholder="Nickname"/>
                         </Form.Group>
                         <br/>
                         <Form.Group>
                             <Form.Label>이메일</Form.Label>
-                            <Form.Control type="email" placeholder="Email"/>
+                            <Form.Control value={email} onChange={onEmailHandler} type="email" placeholder="Email"/>
                         </Form.Group>
                         <br/>
                         <Form.Group>
                             <Form.Label>비밀번호</Form.Label>
-                            <Form.Control type="password" placeholder="Password"/>
+                            <Form.Control value={password} onChange={onPasswordHandler} type="password" placeholder="Password"/>
                         </Form.Group>
                         <br/>
                         <Form.Group>
                             <Form.Label>비밀번호 확인</Form.Label>
-                            <Form.Control type="password" placeholder="Confirm Password"/>
+                            <Form.Control value={confirmPassword} onChange={onConfirmPasswordHandler} type="password" placeholder="Confirm Password"/>
                         </Form.Group>
                         <br/>
-                        <Button block variant="info" type="button" className="my-3">
+                        <Button onClick={onSubmit} block="true" variant="info" type="button" className="my-3">
                             회원가입
                         </Button>
                     </Form>
